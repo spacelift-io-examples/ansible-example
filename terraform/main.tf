4c4494c95e59fd9791289b4d3f9991ee624c954d
Jake <jacobj@spacelift.io>
2025-01-27 18:01:17 -0800

    updated provider config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id = "<your-subscription-id>"
 }
+
 
 resource "azurerm_resource_group" "example" {
   name     = "K21-ResourceGroup"
